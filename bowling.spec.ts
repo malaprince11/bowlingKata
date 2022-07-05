3b205435ad9680e3fd674ed3b220f7acd9f85f87
malamine.bah <malaminebah91@gmail.com>
2022-07-05 16:53:43 +0200

    whip

---
+++ bowling.spec.ts
@@ -0,0 +1,12 @@
+describe("test for bowling rule calculator", () => {
+  it("test n1 a definir ", () => {
+    //given
+    const listeDeTour = [5, 3];
+    const tour1 = 5;
+    const tour2 = 3;
+
+    //when
+
+    //then
+  });
+});
